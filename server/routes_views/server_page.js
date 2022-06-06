e7f53ad0d4f8abb71dd0aab450bdd9d77a04f249
Igor <i_swat@mail.ru>
2022-06-06 20:53:21 +0300

    Перенастроены данные для docker

--- server/routes_views/server_page.js
+++
@@ -1,8 +0,0 @@
-const Route = require('express')
-const router = new Route()
-
-router.get('/', (req, res) => {
-	res.render('serverPage', { title: 'Статичная страница с сервера' })
-})
-
-module.exports = router
