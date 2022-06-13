e62e8a63bd29a842aa37c8837edbb4b58309b489
xIgor1982 <i_swat@mail.ru>
2022-06-13 11:15:23 +0300

    Исправлены настройки сервера

--- client/src/config-server.js
+++ client/src/config-server.js
@@ -1,5 +1,19 @@
-// Для итоговой работы на сервере
-const ENDPOINT_SERVER = 'http://10.0.198.39:3001'
+// Для запуска на итоговом сервере
+export const ENDPOINT_SERVER = 'http://10.0.198.39:3001'
 
-// Для запроса при разработке
+//Для запуска удаленно через тестовый сервер на docker
+// export const ENDPOINT_SERVER = 'http://87.245.175.67:25099'
+
+//Для запуска удаленно через тестовый сервер на docker-synology
+// export const ENDPOINT_SERVER = 'http://87.245.175.67:25099'
+
+//------------------------------------------------------//
+
+// Для запроса при разработке --- локально на компьютере
 // export const ENDPOINT_SERVER = 'http://localhost:3001'
+
+// Для запроса при разработке 2 --- для запуска docker в сети дома
+// export const ENDPOINT_SERVER = 'http://192.168.1.70:3001'
+
+// Для запроса при разработке 3 --- для запуска docker в сети дома synology
+// export const ENDPOINT_SERVER = 'http://192.168.1.55:3001'
