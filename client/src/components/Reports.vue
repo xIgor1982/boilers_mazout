8c59f05306bb31d6b234aad0d46bac5bdc600dc7
xIgor1982 <i_swat@mail.ru>
2022-06-20 22:22:23 +0300

    скорректирован вывод времени

--- client/src/components/Reports.vue
+++ client/src/components/Reports.vue
@@ -114,11 +114,35 @@ export default {
 					const tmpArray = []
 
 					res.forEach(item => {
-						const t = item.dt.slice(0, 10).split('-')
+						const d = item.dt.slice(0, 10).split('-'),
+						      t = item.dt.slice(11, 16).split(':')
+						
+						let s = +t[1], h = +t[0]
+
+						if (h >= 21) {
+							switch (h) {
+								case 21: 
+									h = 0
+									break
+								case 22:
+									h = 1
+									break
+								case 23:
+									h = 2
+									break
+							}
+						} else {
+							h += 3
+						}
+
+						s = `0${s}`.slice(-2)
+						h = `0${h}`.slice(-2)
+
+						console.log(`${h}:${s} мск`)
 
 						tmpArray.push({
-							date: `${t[2]}.${t[1]}.${t[0]} г.`,
-							time: item.dt.slice(11, 16),
+							date: `${d[2]}.${d[1]}.${d[0]} г.`,
+							time: `${h}:${s} "мск"`,
 							value: item.value,
 							idReg: item.id_reg_Registers,
 						})
